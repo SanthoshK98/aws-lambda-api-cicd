46e3923850c2fafc731467c734ea27000988614e
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2023-01-11 14:07:13 +0530

    -

---
+++ app/app.js
@@ -0,0 +1,45 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+// exports.lambdaHandler = async (event, context) => {
+//     try {
+//         // const ret = await axios(url);
+//         response = {
+//             'statusCode': 200,
+//             'body': JSON.stringify({
+//                 message: 'hello world',
+//                 // location: ret.data.trim()
+//             })
+//         }
+//     } catch (err) {
+//         console.log(err);
+//         return err;
+//     }
+
+//     return response
+// };
+app.get('/hello',(req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(5000,()=>{
+    console.log("Listening on Port 3000")
+})
+
+module.exports.handler = serverless(app) 
